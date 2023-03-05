21cae4966aae26696d0565faaf5323d84c5bb0e2
AhmedaliElgabry <53542478+AhmedaliElgabry@users.noreply.github.com>
2023-03-05 12:50:36 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Stepper from "react-stepper-horizontal";
+import Form from "./Form";
+
+//import Stepper from "react-form-stepper";
+
+import "./App.css";
+import CreateTable from "./components/CreateTable";
+import Home from "./components/Home";
+
+const App = () => {
+  const [RowNo, setRowNo] = useState(0);
+  const [ColNo, setColNo] = useState(0);
+  const [dep, setDepName] = useState("");
+  const [month, setMonthName] = useState("");
+  const [year, setYearName] = useState("");
+  return (
+    <>
+      <ToastContainer />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={() => (
+            <Home
+              setRowNo={setRowNo}
+              setMonthName={setMonthName}
+              setDepName={setDepName}
+              setYearName={setYearName}
+              setColNo={setColNo}
+            />
+          )}
+        />
+        {RowNo > 0 && (
+          <Route
+            path="/createTable"
+            component={() => (
+              <CreateTable
+                RowNo={RowNo}
+                dep={dep}
+                month={month}
+                ColNo={ColNo}
+                year={year}
+              />
+            )}
+          />
+        )}
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
+};
+
+export default App;
